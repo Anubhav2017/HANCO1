54f3a26ba5d36b911d43147bd8aeb339970a470f
Anubhav Agarwal <agarwalanubhav.2011@gmail.com>
2018-06-27 22:10:10 +0530

    27/6 10:10pm

--- app/src/main/java/hanco/itsp/android/hanco1/HomeActivity.java
+++ app/src/main/java/hanco/itsp/android/hanco1/HomeActivity.java
@@ -9,12 +9,17 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Size;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     public static CameraManager cameraManager;
@@ -24,10 +29,16 @@ public class HomeActivity extends AppCompatActivity {
     public static CameraCharacteristics rearCamerachars;
     public static Size rearsize;
     public static Size frontsize;
-
-
-
+    public Intent intentocr;
+    public Intent intentlogo;
+    public Intent intentcam;
+    public Intent intentrpi;
     private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final int SPEECH_REQUEST_CODE = 0;
+    public static String spokenText;
+    public TextView speechText;
+
+
 
 
 
@@ -38,6 +49,55 @@ public class HomeActivity extends AppCompatActivity {
         Button ocrbutton=findViewById(R.id.home_ocr);
         Button logobutton=findViewById(R.id.home_logo);
         Button camerabutton=findViewById(R.id.home_camera);
+        Button rpiButton=findViewById(R.id.rpiButton);
+        speechText=findViewById(R.id.speechtext);
+        Button speakButton=findViewById(R.id.speakButton);
+        intentocr=new Intent(HomeActivity.this,OCRActivity.class);
+        intentlogo=new Intent(HomeActivity.this,LogoActivity.class);
+        intentcam=new Intent(HomeActivity.this,CameraActivity.class);
+        intentrpi=new Intent(HomeActivity.this,stream.class);
+
+
+
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                this.displaySpeechRecognizer();
+                if(spokenText=="logo"){
+                    intentlogo=new Intent(HomeActivity.this,LogoActivity.class);
+                    startActivity(intentlogo);
+                }
+                else if(spokenText=="ocr"){
+                    Toast.makeText(HomeActivity.this,spokenText,Toast.LENGTH_LONG);
+                    intentocr=new Intent(HomeActivity.this,OCRActivity.class);
+                    startActivity(intentocr);
+                }
+                else if(spokenText=="raspberry"){
+                    Toast.makeText(HomeActivity.this,spokenText,Toast.LENGTH_LONG);
+                    intentrpi=new Intent(HomeActivity.this,stream.class);
+                    startActivity(intentrpi);
+                }
+                else if(spokenText=="camera"){
+                    intentcam=new Intent(HomeActivity.this,CameraActivity.class);
+                    startActivity(intentcam);
+                }
+                else{
+                    Toast.makeText(HomeActivity.this,"Not recognized",Toast.LENGTH_LONG);
+                }
+
+            }
+            // Create an intent that can start the Speech Recognizer activity
+            private void displaySpeechRecognizer() {
+                Intent intent = new Intent(android.speech.RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(android.speech.RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        android.speech.RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+                startActivityForResult(intent, SPEECH_REQUEST_CODE);
+            }
+
+
+        });
         cameraManager=(CameraManager)getSystemService(CAMERA_SERVICE);
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{
@@ -63,23 +123,42 @@ public class HomeActivity extends AppCompatActivity {
         ocrbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(HomeActivity.this,OCRActivity.class);
-                startActivity(intent);
+                startActivity(intentocr);
             }
         });
         logobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(HomeActivity.this,LogoActivity.class);
-                startActivity(intent);
+                startActivity(intentlogo);
             }
         });
         camerabutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(HomeActivity.this,CameraActivity.class);
-                startActivity(intent);
+                startActivity(intentcam);
+            }
+        });
+        rpiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentrpi);
             }
         });
     }
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            spokenText = results.get(0);
+            // Do something with spokenText
+            speechText.setText(spokenText);
+            Toast.makeText(HomeActivity.this,spokenText,Toast.LENGTH_LONG);
+                    }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
